a70ccb0404dcade2cf85ead199a64af4600455d3
Lee Winfield <winfieldlee01@gmail.com>
2020-02-07 23:37:56 -0600

    can now get documents from s3 and attach them to email

--- email/main.go
+++ email/main.go
@@ -4,8 +4,14 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"net/mail"
 	"net/smtp"
 	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/scorredoira/email"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -25,7 +31,51 @@ var (
 	ErrNon200Response = errors.New("Non 200 Response found")
 )
 
+func getDocument(sess *session.Session, fileName string, key string) error {
+	fmt.Println("Getting Documents!!!!!!!!!!")
+	bucket, err := getParameter(sess, "bill-bucket")
+	if err != nil {
+		return err
+	}
+
+	downloader := s3manager.NewDownloader(sess)
+	file, err := os.Create(fileName)
+	defer file.Close()
+	if err != nil {
+		logrus.Errorf("Error creating file: %v\n", err)
+		return err
+	}
+	numBytes, err := downloader.Download(file,
+		&s3.GetObjectInput{
+			Bucket: aws.String(bucket),
+			Key:    aws.String(key),
+		})
+
+	if err != nil {
+		logrus.Errorf("Error downloading document from s3: %v\n", err)
+		return err
+	}
+
+	fmt.Println("Downloaded", file.Name(), numBytes, "bytes")
+
+	return nil
+}
+
+func getParameter(sess *session.Session, pName string) (string, error) {
+	client := ssm.New(sess)
+	f, err := client.GetParameter(&ssm.GetParameterInput{Name: &pName})
+	if err != nil {
+		logrus.Errorf("Error getting parameter: %v\n", err)
+		return "", err
+	}
+	return *f.Parameter.Value, nil
+}
+
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	fileName := "/tmp/test.pdf"
+	key := "recipient/1001.pdf"
+	recipient := "winfieldlee01@gmail.com"
+
 	host := os.Getenv("SMTP_HOST")
 	port := os.Getenv("SMTP_PORT")
 
@@ -35,38 +85,38 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 		return events.APIGatewayProxyResponse{}, err
 	}
 
-	client := ssm.New(sess)
-
-	be := "billing-email"
-	f, err := client.GetParameter(&ssm.GetParameterInput{Name: &be})
+	err = getDocument(sess, fileName, key)
 	if err != nil {
-		logrus.Errorf("Error getting parameter: %v\n", err)
+		logrus.Errorf("Error getting document: %v\n", err)
 		return events.APIGatewayProxyResponse{}, err
 	}
 
-	bep := "billing-email-password"
-	p, err := client.GetParameter(&ssm.GetParameterInput{Name: &bep})
+	from, err := getParameter(sess, "billing-email")
 	if err != nil {
-		logrus.Errorf("Error getting parameter: %v\n", err)
 		return events.APIGatewayProxyResponse{}, err
 	}
 
-	from := *f.Parameter.Value
-	pass := *p.Parameter.Value
-	recipient := "winfieldlee01@gmail.com"
-
-	msg := "From: " + from + "\n" +
-		"To: " + recipient + "\n" +
-		"Subject: Hello there!\n\n" +
-		"new body"
-
-	err = smtp.SendMail(fmt.Sprintf("%v:%v", host, port),
-		smtp.PlainAuth("", from, pass, host),
-		from, []string{recipient}, []byte(msg))
-
+	pass, err := getParameter(sess, "billing-email-password")
 	if err != nil {
-		log.Printf("smtp error: %s", err)
 		return events.APIGatewayProxyResponse{}, err
+	}
+
+	m := email.NewMessage("Billing", "HI")
+	m.From = mail.Address{Name: "From", Address: from}
+	m.To = []string{recipient}
+
+	// add attachments
+	if err := m.Attach(fileName); err != nil {
+		log.Fatal(err)
+	}
+
+	// add headers
+	m.AddHeader("X-CUSTOMER-id", "xxxxx")
+
+	// send it
+	auth := smtp.PlainAuth("", from, pass, host)
+	if err := email.Send(fmt.Sprintf("%v:%v", host, port), auth, m); err != nil {
+		log.Fatal(err)
 	}
 
 	return events.APIGatewayProxyResponse{
--- go.mod
+++ go.mod
@@ -5,5 +5,7 @@ go 1.13
 require (
 	github.com/aws/aws-lambda-go v1.13.3
 	github.com/aws/aws-sdk-go v1.28.13
+	github.com/scorredoira/email v0.0.0-20191107070024-dc7b732c55da
 	github.com/sirupsen/logrus v1.4.2
+	golang.org/x/net v0.0.0-20200202094626-16171245cfb2 // indirect
 )
